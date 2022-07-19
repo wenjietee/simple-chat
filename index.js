0b95e704cf76ed9b30161e25e3edd09321d9dab0
Tee Wenjie <wenjietj@gmail.com>
2022-07-19 22:58:04 +0800

    init socketio

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 app.use(express.static('public'));
 
 // CONFIG
@@ -9,6 +13,10 @@ const PORT = process.env.PORT || 3000;
 // ROUTES
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+	console.log('a user has connected...');
 });
 
 // LISTEN
